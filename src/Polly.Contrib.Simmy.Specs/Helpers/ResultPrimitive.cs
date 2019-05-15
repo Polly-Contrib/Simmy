89e1a732272d0f3a25cae1c269f39219922b6bb5
Geovanny Alzate Sandoval <vany0114@hotmail.com>
2019-05-14 21:50:44 -0500

    Refactors project name in order to be consistent with Polly.Contrib NuGet organization.

---
+++ src/Polly.Contrib.Simmy.Specs/Helpers/ResultPrimitive.cs
@@ -0,0 +1,17 @@
+﻿namespace Polly.Contrib.Simmy.Specs.Helpers
+{
+    /// <summary>
+    /// A helper class supporting tests on how Policy&lt;TResult&gt; policies may handle return results which are primitive types such as ints or enums.
+    /// </summary>
+    internal enum ResultPrimitive
+    {
+        Undefined,
+        Fault,
+        Good,
+        FaultAgain,
+        GoodAgain,
+        FaultYetAgain,
+        Substitute,
+        WhateverButTooLate
+    }
+}
